696f27b5d2f172ce105cd4d309e1bf02b45721e6
Azizxon8288 <azizxonweb@gmail.com>
2021-11-15 16:59:01 +0500

    add fragment

--- app/src/main/java/com/example/taskerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskerapp/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.taskerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import com.example.taskerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-
-
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
     }
 
 }
